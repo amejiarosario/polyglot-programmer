51d0bc3013f5f69a7bb861a1e67b45bca3b09d9b
Adrian Mejia <adriansky@gmail.com>
2020-07-16 16:37:56 -0400

    all working

--- src/app/data/javascript.ts
+++ src/app/data/javascript.ts
@@ -4,12 +4,85 @@ export default {
     syntax: 'js',
   },
   data: {
-    basics: {
+    cli: {
       showVersion: 'node --version',
-      helloWorld: 'console.log("Hello World");',
+      runningAFile: '$ node foo.js',
+      repl: '$ node',
+      inlineExecution: '$ node -e "console.log("hi!");"',
     },
-    conditionals: {
-      if: 'if (x) doSomthing();',
+    basics: {
+      helloWorld: `// $ cat hello.js
+console.log("Hello World");
+
+// $ node hello.js`,
+    },
+    commments: {
+      singleLine: '// comment',
+      multiLine: `/* line
+another line */`,
+    },
+    variables: {
+      locals: `let x = 1;
+let y = 2, z = 3;`,
+      constants: `const PI = 3.14;
+PI += 1;
+console.log(PI);
+// TypeError: Assignment to constant variable.`,
+      parallelAssigement: `let [x, y, z] = [1, 2, 3];`,
+      swap: `[x, y] = [y, x]`,
+      increments: `x++;
+y--;
+x += 1;
+y -= 1;`,
+    },
+    executionControl: {
+      if: `if (n === 0) {
+  console.log('no hits');
+} else if (n === 1) {
+  console.log('1 hit');
+} else {
+  console.log(n + ' hits');
+}
+
+// also for single line
+
+if (n === 0) console.log('no hits');
+else if (n === 1) console.log('1 hit');
+else console.log(n + ' hits');
+`,
+      switch: `switch (n) {
+  case 0:
+    console.log('no hits');
+    break;
+  case 1:
+    console.log('one hit');
+    break;
+  default:
+    console.log(n + ' hits');
+}`,
+      while: `while (i < 100) {
+  i += 1;
+}`,
+      for: `for (let i = 0; i < 10; ++i) {
+  console.log(i);
+}`,
+      break: `for (let i = 30; i < 50; ++i) {
+  if (i % 7 === 0) {
+    console.log('first multiple: ' + i);
+    break;
+  }
+}
+
+// first multiple: 35`,
+      continue: `for (let i = 30; i < 50; ++i) {
+  if (i % 7 === 0) {
+    continue;
+  }
+  console.log('not divisible: ' + i);
+}
+
+// odd:31
+// odd:33`,
     },
   },
 };
--- src/app/data/python.ts
+++ src/app/data/python.ts
@@ -4,13 +4,61 @@ export default {
     syntax: 'py',
   },
   data: {
-    basics: {
+    cli: {
       showVersion: 'python --version',
-      helloWorld: 'print("Hello World!");',
+      runningAFile: '$ python foo.py',
+      repl: '$ python',
+      inlineExecution: `$ python -c 'print("hi!")'`,
     },
-    conditionals: {
-      if: `if (true):
-doSomething()`,
+    basics: {
+      helloWorld: `# $ cat hello.py
+print ("Hello World!");
+
+# $ python3 hello.py`,
+    },
+    commments: {
+      singleLine: '	# comment',
+      multiLine: `'''comment line
+another line'''`,
+    },
+    variables: {
+      locals: `x = 1
+y, z = 2, 3`,
+      constants: `PI = 3.14
+PI += 1
+print(PI)
+# 4.140000000000001
+# Not enforced`,
+      parallelAssigement: `x, y, z = 1, 2, 3`,
+      swap: `x, y = y, x`,
+      increments: `x += 1
+y -= 1`,
+    },
+    executionControl: {
+      if: `if 0 == n:
+  print('no hits')
+elif 1 == n:
+  print('one hit')
+else:
+  print(str(n) + ' hits')`,
+      switch: `# N/A`,
+      while: `while i < 100:
+  i += 1`,
+      for: `for i in range(1, 11):
+  print(i)`,
+      break: `for i in range(30, 50):
+  if i % 7 == 0:
+    print('first multiple: ' + str(i))
+    break
+
+# first multiple: 35`,
+      continue: `for i in range(30, 35):
+  if i % 2 == 0:
+    continue
+  print('odd:' + str(i))
+
+# odd:31
+# odd:33`,
     },
   },
 };
